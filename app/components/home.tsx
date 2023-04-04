4787547a0c932761659cb510ab0480244f15797e
Enjunlu <73875980+miqiv99@users.noreply.github.com>
2023-04-04 15:20:04 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -627,9 +627,9 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>GPT NEXT</div>
           <div className={styles["sidebar-sub-title"]}>
-            Engine Six AI Assistant
+            Engine Six Copyright
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
